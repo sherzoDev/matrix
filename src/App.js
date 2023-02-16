0a8effead2787ad0b07e5bf7dba735401ac7f52f
shakhzoDev <sherzodmustafoyevmobile@gmail.com>
2023-02-16 18:05:39 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Header from "./components/Header/Header";
 import CourseAbout from "./components/CourseAbout/CourseAbout";
 import "./index.css";
+import Footer from "./components/Footer/Footer";
+import Info from "./components/Info/Info";
+import Tarif from "./components/Tarif/Tarif";
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <CourseAbout />
+      {/* <Header />
+      <CourseAbout /> */}
+      <Tarif />
+      <Info />
+      <Footer />
     </div>
   );
 }
